b5ed4445486d5c6ff2667b4760c5be5ebcdbf2e0
kelmenhorst <kelmenhorst@uni-osnabrueck.de>
2022-02-10 17:42:04 +0100

    runscript: inspect report file rather than output

--- runscript.py
+++ runscript.py
@@ -4,6 +4,7 @@ from urllib.parse import urlparse
 import sys
 import argparse
 import random
+import json
 
 sni_cloudflare = {}
 sni_cloudflare["sni_alt"] = "https://www.cloudflare.com"
@@ -15,17 +16,19 @@ sni_other["sni_alt"] = "https://quic.nginx.org/"
 sni_other["sni_alt_name"] = "quic.nginx.org"
 sni_other["sni_alt_cache"] = "35.214.218.230"
 
-def run_command(entry):
+def run_urlgetter_command(entry):
     cmd = make_urlgetter_command(entry)
     print("RUN ",cmd)
     out = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     out_str = out.stdout.decode("utf-8")
     return out_str
 
-def measurement_passed(output):
-    if "<warn> measurement failed" in output:
-        return False
-    return True
+def run_quicping_command(entry):
+    cmd = make_quicping_command(entry)
+    print("RUN",cmd)
+    out = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    out_str = out.stdout.decode("utf-8")
+    return out_str
 
 def measure(step, url):
     entry = {}
@@ -47,22 +50,22 @@ def measure(step, url):
 
     entry["step"] = step
 
-    out = run_command(entry)
-    return measurement_passed(out), entry
+    run_urlgetter_command(entry)
+    return entry
 
 
-def measure_sni(step, entry):
+def measure_sni(entry):
     sni = sni_other
     entry["step"] = entry["step"]+"_sni"
     entry["sni"] = sni["sni_alt_name"]
-    out = run_command(entry)
+    out = run_urlgetter_command(entry)
 
     # cloudflare QUIC rejects non-cloudflare SNIs. 
     # If we encounter such an error we retry with the cloudflare SNI.
     if "ssl_failed_handshake" in out:
         sni = sni_cloudflare
         entry["sni"] = sni["sni_alt_name"]
-        out = run_command(entry)
+        out = run_urlgetter_command(entry)
 
     entry["step"] = entry["step"]+"_inverse"
     entry["sni"] = entry["domain"]
@@ -70,7 +73,12 @@ def measure_sni(step, entry):
     entry["domain"] = urlparse(sni["sni_alt"]).netloc
     if "dnscache" in entry:
         entry["dnscache"] = sni["sni_alt_cache"]
-    run_command(entry)
+    run_urlgetter_command(entry)
+
+
+def measure_quicping(entry):
+    out = run_quicping_command(entry)
+    
 
      
 def make_urlgetter_command(entry):
@@ -101,7 +109,7 @@ def make_urlgetter_command(entry):
     cmd.append("-A")
     cmd.append("urlgetter_step="+entry["step"])
 
-    # cmd.append("-n") # no collector
+    cmd.append("-n") # no collector
     
     if "no_report" in entry:
         cmd.append("-N")
@@ -109,10 +117,31 @@ def make_urlgetter_command(entry):
     cmd.append("urlgetter")
     return cmd
 
+def make_quicping_command(entry):
+    cmd = [miniooni]
+
+    cmd.append("-i")
+    if "dnscache" in entry:
+        print(entry["dnscache"])
+        cmd.append(entry["dnscache"])
+    else:
+        cmd.append(entry["input_url"])
+
+    cmd.append("-n") # no collector
+    
+    if "no_report" in entry:
+        cmd.append("-N")
+
+    cmd.append("quicping")
+    return cmd
+
 
 def main(urls, dnscache, longm):
-    sni_measure_input = {}
-    keyidx = 0
+    sni_measure_input = []
+    quicping_measure_input = []
+    all_quic = {}
+    all_tcp = {}
+
     # first loop: test urls with local quad8
     tt = len(urls)
     c = 1
@@ -122,16 +151,10 @@ def main(urls, dnscache, longm):
             c += 1
 
             # QUIC
-            passed, entry = measure("quic_local",u)
-            if not passed:
-                sni_measure_input[keyidx] = dict(entry)
-                keyidx += 1
+            entry = measure("quic_local",u)
 
             # TCP
-            passed, entry = measure("tcp_local",u)
-            if not passed:
-                sni_measure_input[keyidx] = dict(entry)
-                keyidx += 1
+            entry = measure("tcp_local",u)
     
     # second loop: test urls with cached ip
     c = 1
@@ -140,21 +163,36 @@ def main(urls, dnscache, longm):
         c += 1
 
         # QUIC
-        passed, entry = measure("quic_cached",u)
-        if not passed:
-            sni_measure_input[keyidx] = dict(entry)
-            keyidx += 1
+        entry = measure("quic_cached",u)
+        all_quic[u] = entry
 
         # TCP
-        passed, entry = measure("tcp_cached",u)
-        if not passed:
-            sni_measure_input[keyidx] = dict(entry)
-            keyidx += 1
+        entry = measure("tcp_cached",u)
+        all_tcp[u] = entry
     
+    with open("report.jsonl", 'r') as dump:
+        lines = dump.readlines()
+    
+    for i,l in enumerate(lines):
+        data = json.loads(l)
+        input_url = data["input"]
+        print(data["test_keys"])
+        failure = data["test_keys"]["failure"]
+        if failure is not None:
+            sni_measure_input.append(all_quic[input_url])
+            sni_measure_input.append(all_tcp[input_url])
+            quicping_measure_input.append(all_quic[input_url].copy())
+
+
     tt = len(sni_measure_input)
-    for c, entry in sni_measure_input.items():
+    for c, entry in enumerate(sni_measure_input):
         print("Last round: Retry failed websites with spoofed SNI "+ str(c+1)+"/"+str(tt))
-        measure_sni(entry["step"], entry)
+        measure_sni(entry)
+    
+    tt = len(quicping_measure_input)
+    for c, entry in enumerate(quicping_measure_input):
+        print("Last round: Retry failed hosts with quicping "+ str(c+1)+"/"+str(tt))
+        measure_quicping(entry)
         
             
 
